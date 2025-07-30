98d2a89f7bac43d4c900db523c90c4d0a51b3e69
Kovalev Oleg <oleglvl80@mail.ru>
2025-07-31 00:30:09 +0300

    API HW v1

--- app/src/main/java/com/example/playlistmaker/Track.kt
+++ app/src/main/java/com/example/playlistmaker/Track.kt
@@ -1,8 +1,17 @@
 package com.example.playlistmaker
 
+import com.google.gson.annotations.SerializedName
+import java.text.SimpleDateFormat
+import java.util.*
+
 data class Track(
     val trackName: String,
     val artistName: String,
-    val trackTime: String,
+    val trackTimeMillis: Long,
     val artworkUrl100: String
-)
+) {
+    val trackTime: String
+        get() {
+            return SimpleDateFormat("mm:ss", Locale.getDefault()).format(trackTimeMillis)
+        }
+}
